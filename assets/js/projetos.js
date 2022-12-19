f462e658866ddc3210d25dc3b49f6f9ee4c0077d
waziri ally amiri <74810402+WAZIRI123@users.noreply.github.com>
2022-12-19 09:56:48 +0300

    test

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -1,4 +1,23 @@
 function projetos(section) {
+    const cards = [{
+            title: "Card 1",
+            details: "Details for card 1",
+            image: "image1.jpg"
+        },
+        {
+            title: "Card 2",
+            details: "Details for card 2",
+            image: "image2.jpg"
+        },
+        {
+            title: "Card 3",
+            details: "Details for card 3",
+            image: "image3.jpg"
+        }
+    ];
+    cards.forEach(function(card) {
+        console.log(card.details)
+    });
     section.innerHTML =
         `<header>
       <h2>Projects</h2>
@@ -44,7 +63,7 @@ function projetos(section) {
           </div>
           <p>
           Grandezza's
-          Booking System is full booking best system that allow the customer to book for trip activities and packages from he Grandezza tour and travel agency.
+          Booking System is full booking  system that allow the customer to book for trip activities and packages from he Grandezza tour and travel agency.
           </p>
         </div>
       </div>
