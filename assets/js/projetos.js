c6b0e8b26b8df1bee1ecfe056a70bedb9509853c
waziri ally amiri <74810402+WAZIRI123@users.noreply.github.com>
2024-04-30 13:21:28 +0300

    wip

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -32,7 +32,7 @@ function projetos(section) {
     cards.forEach(function(element) {
         html += ` <div class="card">
       <div class="img-wrapper">
-      <div data-bg="https://reveccs.com/wp-content/uploads/2023/07/leopard-tours.png" class="bdt-scroll-image bdt-scroll-image-bottom-top rocket-lazyload entered lazyloaded" style="background-image: url(${element.image});" data-ll-status="loaded"></div>
+      <div data-bg="https://reveccs.com/wp-content/uploads/2023/07/leopard-tours.png" class="bdt-scroll-image bdt-scroll-image-bottom-top rocket-lazyload entered lazyloaded" style="background-image: url('https://reveccs.com/wp-content/uploads/2023/07/leopard-tours.png');" data-ll-status="loaded"></div>
 
       <a  href="${element.url}" class="bdt-scroll-image-caption bdt-caption-text">${element.title}</a>
   
