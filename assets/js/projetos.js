0faa4bb05069cb37ef2d6d6db138f5d2791c4a21
waziri ally amiri <74810402+WAZIRI123@users.noreply.github.com>
2024-06-14 09:50:23 -0700

    Update projetos.js

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -4,7 +4,7 @@ function projetos(section) {
       {
         title: "Kilimanjaro Christian Meical Centre",
         details: "Grandezza's Booking System is full booking system that allow the customer to book for trip activities and packages from he Grandezza tour and travel agency.",
-        image: "./assets/images/projects/kcmc.jpg",
+        image: "./assets/images/projects/smartmockups_lxex6mcb.jpg",
         url: "https://www.kcmc.ac.tz/"
     },
       
